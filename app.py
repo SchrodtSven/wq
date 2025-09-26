9f768a63a88d3d5df47bd4ae7aa895b97f44d8af
Sven Schrodt <sven@schrodt.club>
2025-09-26 15:04:37 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,73 @@
+# Whisky Online Shop Data analysis & prediction stuff
+# Projekt "Data on the rox"
+# AUTHOR 
+# SINCE 2025-09-26
+
+# 3rd party libs
+from dash import (
+    Dash,
+    html,
+    page_registry,
+    page_container,
+    clientside_callback,
+    Input,
+    Output,
+    dcc,
+    Patch
+)
+import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+import dash
+from dd import DD
+load_figure_template(["sandstone", "simplex", "minty_dark"])
+
+
+
+# This stylesheet defines the "dbc" class.  Use it to style dash-core-components
+# and the dash DataTable with the bootstrap theme.
+#dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+
+#dbc_css = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+
+
+app = Dash(
+    __name__,
+    use_pages=True,
+    
+    #external_stylesheets=[dbc.themes.FLATLY, dbc_css],#[dbc.themes.MINTY, dbc.icons.FONT_AWESOME]
+    external_stylesheets=[dbc.themes.DARKLY, dbc.icons.FONT_AWESOME],
+    suppress_callback_exceptions=True,
+)
+app.title = '“Data on the rox”: Datenanalyse und Vorhersagen'
+#dash.register_page(__name__)
+navbar = dbc.NavbarSimple(
+    [   
+        dbc.DropdownMenu(
+            [
+                dbc.DropdownMenuItem(DD.pages[page["name"]], href=page["path"])
+                for page in page_registry.values()
+                if page["module"] != "pages.not_found_404"
+            ],
+            menu_variant= 'dark',
+            nav=True,
+            label="Auswahl",
+            
+        ),
+    ],
+    brand=app.title,
+    color="primary",
+    dark=True,
+    className="mb-2",
+)
+
+##app.layout = dbc.Container([navbar, page_container], fluid=True, className="dbc")
+app.layout = dmc.MantineProvider(
+    forceColorScheme="dark",
+    children=[navbar, page_container], 
+    
+)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8888)
+    
---
+++ pages/sessions.py
@@ -0,0 +1,59 @@
+# Sitzungsverfolgung
+from dash import Dash, html, dash_table, dcc, callback, Output, Input, register_page
+import plotly.express as px
+import pandas as pd
+import dash_ag_grid as dag
+from dd import DD
+
+sub_title = "Analyse der Sitzungen"
+df = pd.read_csv('exporte/sessions.csv')
+df['datum'] = pd.to_datetime(df['datum'],format='%Y-%m-%d')
+#print(df.columns)
+#fig_2 = fig = px.line(df_f, x="Jahr", y="Anzahl")
+ 
+# Anfangsbelegung
+#tmp = list(df.columns)
+# Jahr ist obligatorisch (auf der X-Achse)
+#del tmp[0]
+
+
+opt = [{"label": k, "value": k} for k in DD.session_y]
+start_val = DD.session_y
+fig = px.line(df, x="datum", y=start_val)
+
+register_page(__name__)
+
+ 
+@callback(
+    Output(component_id="controls-and-graph", component_property="figure"),
+    Input(component_id="controls-and-check-item", component_property="value"),
+)
+def update_graph(col_chosen):
+    fig = px.line(df, x="datum", y=col_chosen, title=sub_title)
+    return fig
+
+
+layout = html.Div(
+    [
+        html.H3(sub_title),
+        html.H4("Sitzungsverfolgung"),
+        dag.AgGrid(
+            id="main_grid_basic",
+            rowData=df.to_dict("records"),
+            columnDefs=[
+                {"field": x, "headerName": DD.col_transl[x]}
+                for x in df.columns
+            ],  
+            columnSize="responsiveSizeToFit",
+            dashGridOptions={"pagination": True},
+        ),
+        dcc.Dropdown(
+            id="controls-and-check-item",
+            options=opt,
+            value=start_val,
+            multi=True,
+        ),
+        dcc.Graph(figure=fig, id="controls-and-graph"),
+        
+    ]
+)
